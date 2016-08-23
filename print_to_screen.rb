6fd1a8465a42d9f7bfb8f52f0d36bee14fab2be8
Sharon Smith <Sharon Smith>
2016-08-23 13:05:07 -0400

    added % 5 == 0

--- print_to_screen.rb
+++ print_to_screen.rb
@@ -2,6 +2,12 @@ number = 1
 100.times do 	
 	if number % 3 == 0
 		puts "Mined"
+		if number % 5 == 0
+			puts "Minds"
+			if number % 15 == 00
+				puts "Mined Minds"
+			end
+		end
 	else 
 		puts number
 	end
