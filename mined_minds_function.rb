27de8152ca447583c8fab9c0dfa1c5ada93fb11c
Sharon Smith <Sharon Smith>
2016-08-23 16:38:39 -0400

    conditional for 3&5 added

--- mined_minds_function.rb
+++ mined_minds_function.rb
@@ -1,7 +1,9 @@
 def mined_minds(number)
-	if number == 3
+	if number % 3 == 0 && number % 5 == 0 
+		"MinedMinds"
+	elsif number % 3 == 0 
 		"Mined"
-	elsif number == 5
+	elsif number % 5 == 0
 		"Minds"
 	else 
 		number
--- test_mined_minds_function.rb
+++ test_mined_minds_function.rb
@@ -19,11 +19,15 @@ class TestMinedMindsFunction < Minitest::Test
 		assert_equal(7, mined_minds(7))
 	end
 
-	def test_3_returns_Mined
+	def test_number_3_Mined
 		assert_equal("Mined", mined_minds(3))
 	end
 
 	def test_5_returns_Minds
 		assert_equal("Minds", mined_minds(5))
+	end
+
+	def test_3and5_returns_MinedMinds
+		assert_equal("MinedMinds", mined_minds(15))
 	end
 end
