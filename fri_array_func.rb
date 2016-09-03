065183eae32e491c7eb762a1fb5a7c194fd04278
Sharon Smith <Sharon Smith>
2016-09-02 23:21:47 -0400

    still works but not full array yet

--- fri_array_func.rb
+++ fri_array_func.rb
@@ -1,8 +1,27 @@
 def array_mined_minds    
 	my_array = []    
 	(1..100).each do |number|    
-		number = number    
-		my_array << number        
+		if number % 3 == 0 && number % 5 == 0 
+			number = "MinedMinds"
+		elsif number % 3 == 0
+			number = "Mined"	
+		elsif number % 5 == 0
+			number = "Minds"				
+		else 
+			number = number
+		end    
+		my_array << number
+		
 	end
-		my_array
+	        
+	my_array
+
+		
+		
+	# elsif number % 3 == 0 
+	# 	"Mined"
+	# elsif number % 5 == 0
+	# 	"Minds"
+	# else 
+	# 	number
 end
--- fri_array_test.rb
+++ fri_array_test.rb
@@ -2,21 +2,23 @@ require "minitest/autorun"
 require_relative "fri_array_func.rb"
 
 class TestArrayFunction <Minitest::Test    
-def test_array_with_100_elements        
-	results = array_mined_minds        
-	assert_equal(100, results.length)    
+	def test_array_with_100_elements        
+		results = array_mined_minds        
+		assert_equal(100, results.length)    
 	end
-end
 
-				
+	def test_3_returns_mined
+ 		results = array_mined_minds
+ 		assert_equal("Mined", results[2])
+ 	end
 
+	def test_5_returns_Minds
+		results = array_mined_minds
+ 		assert_equal("Minds", results[4])
+ 	end
 
-
-
-					
-					
-
-
-
-
-		
+ 	def test_3and5_returns_MinedMinds
+ 		results = array_mined_minds
+ 		assert_equal("MinedMinds", results[14])
+ 	end
+end		
