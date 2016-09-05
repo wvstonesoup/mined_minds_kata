1055c08ae0827eafb3d3ee89f56bc09b3c352d92
Sharon Smith <Sharon Smith>
2016-09-05 14:41:21 -0400

    the mapping finally worked where the each would not

--- fri_array_func.rb
+++ fri_array_func.rb
@@ -1,6 +1,5 @@
-def array_mined_minds    
-	my_array = []    
-	(1..100).each do |number|    
+def array_mined_minds
+	my_array = [*1..100].map do  |number|    
 		if number % 3 == 0 && number % 5 == 0 
 			number = "MinedMinds"
 		elsif number % 3 == 0
@@ -9,19 +8,8 @@ def array_mined_minds
 			number = "Minds"				
 		else 
 			number = number
-		end    
-		my_array << number
-		
+		end
 	end
-	        
-	my_array
-
-		
-		
-	# elsif number % 3 == 0 
-	# 	"Mined"
-	# elsif number % 5 == 0
-	# 	"Minds"
-	# else 
-	# 	number
 end
+
+	
--- fri_array_test.rb
+++ fri_array_test.rb
@@ -7,7 +7,12 @@ class TestArrayFunction <Minitest::Test
 		assert_equal(100, results.length)    
 	end
 
-	def test_3_returns_mined
+	def test_3and5_returns_MinedMinds
+ 		results = array_mined_minds
+ 		assert_equal("MinedMinds", results[14])
+ 	end
+
+ 	def test_3_returns_mined
  		results = array_mined_minds
  		assert_equal("Mined", results[2])
  	end
@@ -17,8 +22,5 @@ class TestArrayFunction <Minitest::Test
  		assert_equal("Minds", results[4])
  	end
 
- 	def test_3and5_returns_MinedMinds
- 		results = array_mined_minds
- 		assert_equal("MinedMinds", results[14])
- 	end
+ 	
 end		
