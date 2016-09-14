c17d086d98d0b4a3672286f1f8ef4b3ebee682b1
Sharon Smith <Sharon Smith>
2016-09-14 19:37:42 -0400

    final code after worked in class

---
+++ coin_changer_func.rb
@@ -0,0 +1,7 @@
+def coins_for_change(cents_received)
+	
+		coins = {}
+
+end
+
+#coins = {:quarter => 25, :dime => 10, :nickel => 5, :penny => 1}
--- test_coin_changer.rb
+++ test_coin_changer.rb
@@ -1,2 +1,18 @@
 #coin changer kata. write a function that takes a number of cents, and that returns a hash the least number of coins needed to generate the value of cents. eg if you have 11 cents you should return {:dime => 1, :penny =?1}; if you have 31 cents you should return {:quarter =>1, :nickel =>1, :penny => 1}
 #build up your coin_changer using tests. commit to git after every test
+
+require "minitest/autorun"
+require_relative "coin_changer_func.rb"
+
+class TestCoinChanger < Minitest::Test
+
+	def test_ret_no_coins
+	cents_received = 0
+
+	assert_equal({}, coins_for_change(cents_received))
+	end
+
+	# def test_one_penny_is_one_cent
+	# 	cents_received  1
+	# 	assert_equal
+end
